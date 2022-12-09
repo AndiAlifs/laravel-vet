305883f1235e995d874db66e7833384948b5651a
AndiAlifs <andyalyfsyah4@gmail.com>
2022-12-09 12:57:35 +0700

    set seeder

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Account;
 
 class AccountSeeder extends Seeder
 {
@@ -13,6 +14,56 @@ class AccountSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $dataSeed = [
+            [
+                'nama' => 'Budi',
+                'saldo' => 1000000,
+                'norek' => '1234567890'
+            ],
+            [
+                'nama' => 'Andi',
+                'saldo' => 2000000,
+                'norek' => '0987654321'
+            ],
+            [
+                'nama' => 'Caca',
+                'saldo' => 3000000,
+                'norek' => '1234567891'
+            ],
+            [
+                'nama' => 'Dedi',
+                'saldo' => 4000000,
+                'norek' => '0987654322'
+            ],
+            [
+                'nama' => 'Euis',
+                'saldo' => 5000000,
+                'norek' => '1234567892'
+            ],
+            [
+                'nama' => 'Fafa',
+                'saldo' => 6000000,
+                'norek' => '0987654323'
+            ],
+            [
+                'nama' => 'Gigi',
+                'saldo' => 7000000,
+                'norek' => '1234567893'
+            ],
+            [
+                'nama' => 'Hani',
+                'saldo' => 8000000,
+                'norek' => '0987654324'
+            ],
+            [
+                'nama' => 'Icha',
+                'saldo' => 9000000,
+                'norek' => '1234567894'
+            ]
+        ];
+        
+        foreach ($dataSeed as $data) {
+            Account::create($data);
+        }
     }
 }
