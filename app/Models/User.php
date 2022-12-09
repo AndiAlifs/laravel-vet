8a66b144ec2f898f7e81954a6db1464983c514b2
AndiAlifs <andyalyfsyah4@gmail.com>
2022-12-09 13:35:35 +0700

    jwt installation

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -37,4 +39,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // jwt key setup
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
